5aa3353d2329ffaac535c21e3149f47d8e4b91ac
beepscore <support@beepscore.com>
2009-11-29 17:07:43 -0800

    Initial commit.  Ref Dudney iPhone SDK Development Ch 05.

---
+++ Classes/MovieTableAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MovieTableAppDelegate.h
+//  MovieTable
+//
+//  Created by Steve Baker on 11/29/09.
+//  Copyright Beepscore LLC 2009. All rights reserved.
+//
+
+@interface MovieTableAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
---
+++ Classes/RootViewController.h
@@ -0,0 +1,12 @@
+//
+//  RootViewController.h
+//  MovieTable
+//
+//  Created by Steve Baker on 11/29/09.
+//  Copyright Beepscore LLC 2009. All rights reserved.
+//
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
