129920b3602753783d429cb6697d4bb88d6a8cfe
beepscore <support@beepscore.com>
2009-11-29 23:48:31 -0800

    In .git/config, renamed remote repository (at github) from MovieTable to MovieTable02.
    Change project name in comments.

--- Classes/MovieTableAppDelegate.m
+++ Classes/MovieTableAppDelegate.m
@@ -1,6 +1,6 @@
 //
 //  MovieTableAppDelegate.m
-//  MovieTable
+//  MovieTable02
 //
 //  Created by Steve Baker on 11/29/09.
 //  Copyright Beepscore LLC 2009. All rights reserved.
--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -1,6 +1,6 @@
 //
 //  RootViewController.m
-//  MovieTable
+//  MovieTable02
 //
 //  Created by Steve Baker on 11/29/09.
 //  Copyright Beepscore LLC 2009. All rights reserved.
